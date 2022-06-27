160d62ba8cd21a0d1ed4b5857b9f78d2f3c05789
Yeonho Jang <siabard@gmail.com>
2022-06-27 14:30:09 +0900

    initial project setup

---
+++ metro.config.js
@@ -0,0 +1,23 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+const defaultAssetExts =
+  require('metro-config/src/defaults/defaults').assetExts;
+
+module.exports = {
+  resolver: {
+    assetExts: [...defaultAssetExts, 'vrx'],
+  },
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+};
